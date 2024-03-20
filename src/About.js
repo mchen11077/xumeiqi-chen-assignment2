cf55d6a1f7ff3508c1825f72e679d44fd44ca821
mchen1107 <chen.xum@northeastern.edu>
2024-03-19 21:27:27 -0700

    add changes

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ const About = () => {
     <div>
       <h1>Credits</h1>
       <p>This Game of Life implementation is created by xxx.</p>
-      <p>Find the source code on GitHub: <a href="https://github.com/xxx/your-repo">GitHub Repo</a></p>
+      <p>Find the source code on GitHub: <a href="https://github.com/mchen11077/xumeiqi-chen-assignment2">GitHub Repo</a></p>
     </div>
   );
 };
