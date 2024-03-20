7fc7126bf4e364f9801e6e6432a22f4a96b540ea
mchen11077 <105175273+mchen11077@users.noreply.github.com>
2024-03-20 01:25:42 -0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -3,8 +3,8 @@ const About = () => {
   return (
     <div>
       <h1>Credits</h1>
-      <p>This Game of Life implementation is created by xxx.</p>
-      <p>Find the source code on GitHub: <a href="https://github.com/xxx/your-repo">GitHub Repo</a></p>
+      <p>This Game of Life implementation is created by XC.</p>
+      <p>Find the source code on GitHub: <a href="https://github.com/mchen11077/xumeiqi-chen-assignment2">GitHub Repo</a></p>
     </div>
   );
 };
